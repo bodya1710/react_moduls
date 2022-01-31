254048a44e7d7839bd87426823b1c9dfbb3e194e
bodya1710 <bodyabordiyk@gmail.com>
2022-01-31 15:26:07 +0200

    class_work4

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
+import {useReducer} from "react";
+
+const reducer = (state, action) => {
+    state = {...state, count2: state.count2 + 1}
+    if (action.type === 'inc'){
+      return   {...state, count1:state.count1 + 1}
+    }    if (action.type === 'dec'){
+        return   {...state, count1:state.count1 - 1}
+    } if (action.type === 'reset'){
+        return   {...state, count1: action.payload, count2:action.payload }
+    }
+
+  return state
+}
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, {count1:0, count2:0});
+
   return (
      <div>
+       <div>{state.count1}</div>
+       <div>{state.count2}</div>
+       <button onClick={()=> dispatch({type:'inc'})}>INC</button>
+       <button onClick={()=> dispatch({type:'dec'})}>DEC</button>
+       <button onClick={()=> dispatch({type:'reset',payload:0})}>RESET</button>
     </div>
   );
 }
